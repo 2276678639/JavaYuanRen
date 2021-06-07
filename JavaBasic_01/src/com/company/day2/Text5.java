4b4da1f544ddd3e4ff5cffeaaf40c1896938221e
2276678639 <qianyiyun005@outlook.com>
2021-06-07 17:49:35 +0800

    作业6

--- JavaBasic_01/src/com/company/day2/Text5.java
+++ JavaBasic_01/src/com/company/day2/Text5.java
@@ -1,7 +1,30 @@
 package com.company.day2;
 
+import java.util.Scanner;
+
 public class Text5 {
     public static void main(String[] args) {
+        /**
+         * 手动输入三个值，求最大值
+         */
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("请输入第一个数：");
+        int num1=scanner1.nextInt();
+        System.out.println("请输入第二个数：");
+        int num2=scanner2.nextInt();
+        System.out.println("请输入第三个数：");
+        int num3=scanner3.nextInt();
+        int max=num1;
+        if(max<num2){
+            max=num2;
+        }
+        if(max<num3){
+            max=num3;
+        }
+        System.out.println("最大值为："+max);
+
 
     }
 }
---
+++ JavaBasic_01/src/com/company/day2/Text6.java
@@ -0,0 +1,28 @@
+package com.company.day2;
+
+import java.util.Scanner;
+
+public class Text6 {
+    public static void main(String[] args) {
+        /**
+         * 手动输入0~9之间的数，求出现的偶数的次数，如果输入的数小于0或者是大于9，程序结束（向控制台输出：您输入的数据过大）
+         */
+        int num = 0;
+        int num1 = 0;
+        do {
+            Scanner scanner1 = new Scanner(System.in);
+            System.out.println("请输入一个数字");
+            int i = scanner1.nextInt();
+            num = i;
+            if (i % 2 == 0) {
+                num1++;
+            }
+        } while
+        (num >= 0 && num <= 9);
+
+
+        System.err.println("您输入的数据过大");
+
+        System.out.println("偶数的数量为：" + num1);
+    }
+}
